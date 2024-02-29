f2b2ff160630582b4552915814c094892019df94
anita98766 <141495916+anita98766@users.noreply.github.com>
2024-02-29 10:35:00 +0530

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-    def hello():
-        return "Hello World!"
+@app.route('/')
+def hello_world():
+    return 'Hello, world'
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 
 
